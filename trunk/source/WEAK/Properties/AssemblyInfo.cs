5f260be34b1b55b3bb44e2836dc3d07c08dd7b86
Doraku_cp <laszlo.paillat@gmail.com>
2017-02-07 11:38:54 +0000

    - edited for nuget packaging

--- trunk/source/WEAK/Properties/AssemblyInfo.cs
+++ trunk/source/WEAK/Properties/AssemblyInfo.cs
@@ -1,8 +1,10 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("WEAK")]
+[assembly: AssemblyTitle("WEAK Core")]
 [assembly: AssemblyDescription(
 @"Core functionalities of WEAK
+Communication
+    - Publisher/Subscriber pattern implementation
 
 Helper
     - Extension method to create a weak delegate from a delegate, so it doesn't hold strong reference on its target
@@ -10,7 +12,10 @@ Helper
     - Extensions methods on object to check input parameters
 
 Input
-    - Command pattern implementation with numerous extensions to handle most commonly used operations and objects (ICollection IList, IDictionary, value setting, custom action)
+    - Command pattern implementation with numerous extensions to handle most commonly used operations and objects (ICollection, IList, IDictionary, value setting, custom action)
+
+Object
+    - IOC pattern, object factory
 
 Serialization
     - Methods to handle load/save operations on DataContract decorated classes")]
